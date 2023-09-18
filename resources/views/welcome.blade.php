a2a6e749a116499e8f035b6bfaf12d9ff063b650
marekgacek45 <marekgacek45@gmail.com>
2023-09-18 19:45:42 +0200

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Instalacja Tailwind</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="flex justify-center items-center mt-12 bg-green-500">
+
+        <h1 class="text-6xl text-red-600">test</h1>
+    </div>
+
+</body>
+</html>
